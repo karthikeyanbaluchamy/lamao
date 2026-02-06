a662890141e180bf534c81ab5485971177fdd201
karthikeyan <55027856+karthikeyanbaluchamy@users.noreply.github.com>
2026-02-06 12:39:49 -0500

    Process SNS messages in lambda_handler

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,6 +2,9 @@ import requests
 
 def lambda_handler(event, context):
     # Example API request
+    for record in event['Records']:
+        sns_message = record['Sns']['Message']
+        print(f"Received message: {sns_message}")
     print("hello")
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
     
